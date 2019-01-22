003e5e84f89120f4ce1d6366f3749218860dbeed
Eric Crosson <eric.s.crosson@utexas.edu>
2019-01-22 09:19:40 -0600

    Use functional example

--- example/client.ts
+++ example/client.ts
@@ -4,12 +4,15 @@ import Record from 'timeseries-record'
 import { BitmexAPI } from 'bitmex-node'
 const bitmex = new BitmexAPI()
 
-(async () => {
-    const quotes = await bitmex.Trade.getBucketed({
+async function main() {
+    const quotes  = await bitmex.Trade.getBucketed({
         binSize: '1d',
         partial: false,
         symbol: 'XBTUSD',
         reverse: true
     })
-    const records: Record[] = quotes.map(bitmexCandleToRecord)
-}) ()
+    const records: Record[] = quotes.map((quote) => bitmexCandleToRecord(quote, '1d'))
+    console.log(quotes.reverse())
+}
+
+main()
